70378b82d1bf716899dda8a5ecbe303b8ba6444f
Paul Gapud <campusinvasionproject@gmail.com>
2025-03-12 15:22:19 +0800

    [usertable] add role-badge

--- frontend/src/components/UserTable.jsx
+++ frontend/src/components/UserTable.jsx
@@ -7,6 +7,9 @@ import EditAdminModal from "./EditAdminModal";
 import ResetPasswordModal from "./ResetPasswordModal";
 import { resetUserPassword, resetAdminPassword } from "../services/authService";
 
+import "../index.css"
+import { Mail } from "lucide-react";
+
 const UserTable = ({ users, onEdit, onDelete, suborganizations }) => {
     const [selectedUser, setSelectedUser] = useState(null);
     const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
@@ -148,8 +151,15 @@ const UserTable = ({ users, onEdit, onDelete, suborganizations }) => {
                                     className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
                                 >
                                     <td className="px-6 py-4">{index + 1}</td>
-                                    <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        {user.firstname} {user.lastname}
+                                    <td className="px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                                    <div className="flex gap-x-6 justify-between items-center">
+                                    	<div className="font-semibold ">{user.firstname} {user.lastname}</div>
+                                        <div className="flex items-center gap-x-1">
+                                        	<div className={`role-badge ${user.role}`}>{user.role}</div>
+																					<Mail className="w-5 h-4 " />
+                                        </div>
+                                        
+                                    </div>
                                     </td>
                                     <td className="px-6 py-4">{user.email}</td>
 
