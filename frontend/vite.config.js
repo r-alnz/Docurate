493cebd3c08e49ef39816e7678ac180f4a6f9ab5
Paul Gapud <campusinvasionproject@gmail.com>
2025-03-25 19:10:27 +0800

    fix: frontend xlsx deps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -12,6 +11,11 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api')
       }
+    }
+  },
+  build: {
+    rollupOptions: {
+      external: ['xlsx']
     }
   }
 })
